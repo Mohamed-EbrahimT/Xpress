ce7aa504e5b6cba161163cad876e3755f495995f
Mohamed-EbrahimT <eyad605@yahoo.com>
2025-04-27 16:58:48 +0300

    Initial commit of MVC project

---
+++ FinalProj/Models/Order.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProj.Models;
+
+[Table("Order")]
+public partial class Order
+{
+    [Key]
+    public int OrderId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public int? CartId { get; set; }
+
+    public DateOnly? OrderDate { get; set; }
+
+    [Column(TypeName = "decimal(10, 2)")]
+    public decimal? OrderAmount { get; set; }
+
+    public int? OrderStatusId { get; set; }
+
+    public DateOnly? ShippingDate { get; set; }
+
+    [ForeignKey("CartId")]
+    [InverseProperty("Orders")]
+    public virtual Cart? Cart { get; set; }
+
+    [InverseProperty("Order")]
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    [ForeignKey("OrderStatusId")]
+    [InverseProperty("Orders")]
+    public virtual OrderStatus? OrderStatus { get; set; }
+
+    [InverseProperty("Order")]
+    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+    [ForeignKey("UserId")]
+    [InverseProperty("Orders")]
+    public virtual User? User { get; set; }
+}
---
+++ FinalProj/Models/Payment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalProj.Models;
+
+[Table("Payment")]
+public partial class Payment
+{
+    [Key]
+    public int PaymentId { get; set; }
+
+    public int? OrderId { get; set; }
+
+    public int? UserId { get; set; }
+
+    public DateOnly? PaymentDate { get; set; }
+
+    [StringLength(50)]
+    public string? PaymentMethod { get; set; }
+
+    [ForeignKey("OrderId")]
+    [InverseProperty("Payments")]
+    public virtual Order? Order { get; set; }
+
+    [ForeignKey("UserId")]
+    [InverseProperty("Payments")]
+    public virtual User? User { get; set; }
+}
